c345cfcab656a575d4e2abba73ce29e3e03e004b
RezaBakhshiNia <reza.bakhshi1381r@outlook.com>
2024-06-14 13:47:50 +0330

    Import default Samim font

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,24 @@
-import Head from "next/head";
 import "@/assets/styles/globals.css";
 import Navbar from "@/components/Navbar";
+import localFont from "next/font/local";
+// import Samim from '@/assets/fonts/Samim.woff';
+// import SamimBold from '@/assets/fonts/Samim-Bold.woff';
+
+// Font files can be colocated inside of `app`
+const samim = localFont({
+  src: [
+    {
+      path: "../assets/fonts/Samim-Bold.woff",
+      weight: "400",
+      style: "bold",
+    },
+    {
+      path: "../assets/fonts/Samim.woff",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+});
 
 export const metadata = {
   title: "خونمون | خونه جدید با خونمون",
@@ -10,8 +28,8 @@ export const metadata = {
 
 const MainLayout = ({ children }) => {
   return (
-    <html lang="fa" dir="rtl">
-      <body id="body">
+    <html lang="fa" dir="rtl" className={samim.className}>
+      <body id="body" className={samim.className}>
         <Navbar />
         <main className="root">{children}</main>
       </body>
